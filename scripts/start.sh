e19004b34cabe6026166f072643909c6f6ac841c
Mohammed Naser <mnaser@vexxhost.com>
2023-10-10 12:55:37 -0400

    use built-in user

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,25 +6,13 @@ RUNNER_VERSION=${RUNNER_VERSION:-2.309.0}
 RUNNER_CHECKSUM=${RUNNER_CHECKSUM:-2974243bab2a282349ac833475d241d5273605d3628f0685bd07fb5530f9bb1a}
 RUNNER_JITCONFIG=___JIT_CONFIG___
 
-# Create "runner" group if it doesn't exist
-if ! getent group ${RUNNER_GROUP}; then
-    groupadd -r ${RUNNER_GROUP}
-fi
-
-# Create "runner" user if it doesn't exist
-if ! getent passwd ${RUNNER_USER}; then
-    useradd -r -g ${RUNNER_USER} -m -d /runner -c "GitHub Actions Runner" runner
-fi
-
-# Add "runner" user to the sudoers
-echo "runner ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-
 # Download the runner package
-cd /runner
+mkdir -p /opt/github/actions-runner/${RUNNER_VERSION}
+cd /opt/github/actions-runner/${RUNNER_VERSION}
 curl -o actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
 echo "${RUNNER_CHECKSUM}  actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
-chown -R ${RUNNER_USER}:${RUNNER_GROUP} /runner
+chown -R ${RUNNER_USER}:${RUNNER_GROUP} /opt/github/actions-runner
 
 # Start the runner
-sudo -u runner -s -- nohup ./run.sh --jitconfig ${RUNNER_JITCONFIG} &
+sudo -u ${RUNNER_USER} -s -- nohup ./run.sh --jitconfig ${RUNNER_JITCONFIG} &
